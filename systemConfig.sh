a7a06f4d53a978e010b850a0586cd12001e557aa
Ronildo Pinheiro de Araujo Moura <ronildo@ppgsc.ufrn.br>
2017-09-18 18:49:53 -0300

    add move gits files.

--- systemConfig.sh
+++ systemConfig.sh
@@ -9,6 +9,8 @@ sudo apt upgrade -y
 sudo apt-get install -y git curl htop
 
 #setup new terminal themes in bashrc
+
+mv git-completion.bash ~/git-completion.bash
 echo '# Enable tab completion' >> ~/.bashrc
 echo 'source ~/git-completion.bash' >> ~/.bashrc
 
@@ -19,6 +21,7 @@ echo 'purple="\[\033[0;35m\]"' >> ~/.bashrc
 echo 'reset="\[\033[0m\]"' >> ~/.bashrc
 
 echo '# Change command prompt' >> ~/.bashrc
+mv git-prompt.sh ~/git-prompt.sh
 echo 'source ~/git-prompt.sh' >> ~/.bashrc
 echo 'export GIT_PS1_SHOWDIRTYSTATE=1' >> ~/.bashrc
 echo "# '\u' adds the name of the current user to the promp'" >> ~/.bashrc
@@ -32,7 +35,7 @@ curl -0 https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3*.sh -b
 rm Miniconda3*.sh
 
-echo -e '\n# Miniconda3\nexport PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
+#echo -e '\n# Miniconda3\nexport PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 echo 'Conda installed! Go in the projects and creat virtual env for yml projects'
