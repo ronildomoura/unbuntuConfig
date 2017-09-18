adb45d19113a1598ac5c6bf6e6b85c964ffac3b3
Ronildo Pinheiro de Araujo Moura <ronildo@ppgsc.ufrn.br>
2017-09-18 19:20:55 -0300

    executable scripts

--- systemConfig.sh
+++ systemConfig.sh
@@ -31,12 +31,12 @@ echo 'export PS1="$purple\u$green\$(__git_ps1)$blue \W $ $reset"' >> ~/.bashrc
 
 
 #get the latest conda 
-curl -0 https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3*.sh -b
-rm Miniconda3*.sh
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#bash Miniconda3*.sh -b
+#rm Miniconda3*.sh
 
 #echo -e '\n# Miniconda3\nexport PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+#source ~/.bashrc
 
 echo 'Conda installed! Go in the projects and creat virtual env for yml projects'
 
