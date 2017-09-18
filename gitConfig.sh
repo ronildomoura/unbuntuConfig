8882c8a3896b564638528b0e6ae72dc506387815
Ronildo Pinheiro de Araujo Moura <ronildo@ppgsc.ufrn.br>
2017-09-18 16:25:56 -0300

    change strings

--- gitConfig.sh
+++ gitConfig.sh
@@ -11,4 +11,4 @@ git config --global credential.helper store
 #clone the config repo
 git clone https://github.com/ronildomoura/unbuntuConfig.git
 
-echo 'Run other Shell config sripts now ...'
+echo 'Run other scripts in main directory ....'
